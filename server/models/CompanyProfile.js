8b133412f7f43550fc993a3532470cd6a8f52151
Gamini GGMT GaminiGSK <gamini.vat80@gmail.com>
2026-01-17 10:47:27 +0700

    fix: add missing user field to CompanyProfile schema

--- server/models/CompanyProfile.js
+++ server/models/CompanyProfile.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
 const CompanyProfileSchema = new mongoose.Schema({
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }, // Link to User Model
     companyCode: { type: String, required: true, unique: true },
     companyNameEn: { type: String },
     companyNameKh: { type: String },
